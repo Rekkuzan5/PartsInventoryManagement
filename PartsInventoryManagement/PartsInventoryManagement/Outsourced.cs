6146d93e48fa5f77e693e83744404323498ccb9e
Rekkuzan5 <cstapp88@gmail.com>
2020-11-18 21:44:44 -0600

    Updated Outsourced class

--- PartsInventoryManagement/PartsInventoryManagement/Outsourced.cs
+++ PartsInventoryManagement/PartsInventoryManagement/Outsourced.cs
@@ -11,6 +11,13 @@ namespace PartsInventoryManagement
         // field for Inhouse class
         public string companyName;
 
+        // 7-parameter constructor
+        public Outsourced(int partID, string name, decimal price, int inStock, int min, int max, string companyName) 
+            : base(partID, name, price, inStock, min, max)
+        {
+            CompanyName = companyName;
+        }
+
         // property for Inhouse class
         public string CompanyName
         {
